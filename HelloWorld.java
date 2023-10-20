27875658a2057289779e726bbe1347a0d7f93eb5
sasirekha <sasi.sid17@gmail.com>
2023-10-20 09:29:06 +0530

    Azuredevops-sample-helloworld-project

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("------****Hello World & welcome toAzure Devops Learning *****......");
+    System.out.println("------****Source repository is Git hub *****......");
+  }
+}
